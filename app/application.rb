cb56f14a3d7b36fc67d25b72d143647073bfa3b8
github username <github email address>
2021-04-19 17:51:01 -0400

    commit tree 79f759754c56e098847c745cb9f2dcd82135cc10

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,5 @@
 class Application
-
+  require 'pry'
   @@items = ["Apples","Carrots","Pears"]
   @@cart = []
   
@@ -11,21 +11,31 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+      binding.pry
+    elsif req.path.match(/cart/)
+      if  @@cart == []
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
+
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-      #####
-        if search_term.match(/cart/)
-          @@cart.each do |item|
-            resp.write "#{item}\n"
-          end
-          if search_term = []
-            resp.write "Your cart is empty"
+        
+    elsif req.path.match(/add/)
+      added_term = req.params["item"]
+        @@items.each do |item|
+          if added_term == item
+            @@cart << item
+          resp.write "added #{item}"
           else
-            #####
+            resp.write "We don't have that item"
           end
         end
-        
+
     else
       resp.write "Path Not Found"
     end
